7a7296a4d7094dd1af8d27e6929c9322fc5c41fb
matingh-gh <matinghorbanloo@gmail.com>
2025-09-08 12:03:28 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: { appDir: true },
-  headers: async () => [
-    {
-      source: "/(.*)",
-      headers: [{ key: "Cache-Control", value: "no-store" }]
-    }
-  ]
-};
-export default nextConfig;
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_DEFAULT_LOCALE: process.env.NEXT_PUBLIC_DEFAULT_LOCALE || 'en',
+  },
+}
+
+export default nextConfig
